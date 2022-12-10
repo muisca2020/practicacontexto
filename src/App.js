4314eb1cdd8c4efc78560b5020d0924149ec13ff
Leonardo Sanchez <leonsanc@gmail.com>
2022-12-10 11:22:48 -0600

    Esta es la definición básica de un contexto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// Con esta declaración se crea un contexto que queda exportado para toda la 
+// aplicación
+export const UserContext = React.createContext();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // Aquí se le asigna un valor al contexto llamado UserContext
+    <UserContext.Provider value="Leo"> 
+      <User/>
+    </UserContext.Provider>
   );
+}
+
+function User(){
+  return (
+    // Aquí se consume el contenido de la variable de contexto llamada
+    // UserContext
+    <UserContext.Consumer>
+      { value => <h1>{value}</h1> }
+    </UserContext.Consumer>
+  )
 }
 
 export default App;
