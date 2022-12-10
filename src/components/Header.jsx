207fcfb5af19ec088dc4d6f0df14fe87e2653450
Leonardo Sanchez <leonsanc@gmail.com>
2022-12-10 13:04:38 -0600

    Llamado a metodos del ciclo de vide del componente

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+class Header extends React.Component {
+    constructor(){
+        super(props);
+        this.state = {favoriteColor: "red"};
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        return {favoriteColor: props.favoriteColor};
+    }
+
+    componentDidMount () {
+        setTimeout(() => {
+            this.setState({favoriteColor: "yellow"});
+        }, 1000)
+    }
+
+    render(){
+        return (
+            <h1> Mi color favorito es { this.state.favoriteColor }</h1>
+        );
+    }
+}
+
+ReactDOM.render(<Header/>, document.getElementById('root'));
